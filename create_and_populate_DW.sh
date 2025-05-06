64c8a4b2f40f0ba11747c07de3a1340dac21a262
Henry-Shearman <henryshearman219.certs@outlook.com>
2025-05-06 10:52:21 +1000

    Bash script to create and load a data warehouse from generated dummy student grade data.

--- create_and_populate_DW.sh
+++ create_and_populate_DW.sh
@@ -4,10 +4,14 @@ db_name=$DB_NAME #Assign a name for application database from environment variab
 db_username=$DB_USERNAME #Assign the username for application database from environment variable. Should be current user.
 db_password=$DB_PASSWORD #Assign password for user in application database from environment variable.
 
+
+#Create dummy student grade data
 python3 create_dummy_data.py
 mv StudentDummyData.csv /tmp
 sudo chmod 444 /tmp/StudentDummyData.csv
 
+
+#Create database and user provisions
 sudo -u postgres dropdb --if-exists $db_name
 sudo -u postgres createdb $db_name
 
@@ -20,6 +24,8 @@ psql -U $db_username -d $db_name -c "CREATE SCHEMA class AUTHORIZATION ${db_user
 
 echo "All privileges granted for user ${db_username} on ${db_name} database"
 
+
+#Create and load data warehouse from SQL script
 psql -U $db_username -d $db_name -f "LoadStudentGrades.sql"
 
 rm -f /tmp/StudentDummyData.csv
